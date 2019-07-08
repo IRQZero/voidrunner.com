3e9ec2430fc66f36e74cfecf1393b9f6f52836f5
interrupt <misterinterrupt@gmail.com>
2019-07-08 00:57:48 -0700

    adjusted blend modes and opening animations

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -13,22 +13,13 @@ window.onload = function () {
   .kaleid(24)
   .scrollY(0.5, 1.4)
   .mask(
-    osc(512,({time}) => Math.sin(time/24) + 0.9 ) // offset
-    .kaleid(3), 1, 0.5)// end mask
-  .rotate(0.4)
-  .scrollX(1, 9)
+  	osc(512,({time}) => Math.sin(time/24) + 0.9 ) // offset
+  	.kaleid(3), [1,6], 0.5)// end mask
+	.scrollX(1, 0.09)
+	.scrollY(1, .333)
   .out(o1);
-osc([14], [2]).rotate([14], [.04].fast(0.5), [7], [0.02].fast(20)).blend(o1, 7).out(o2);
-osc(36).modulatePixelate(o1, 18).scrollX(0.3, [0.3, 3]).mult(o2, 256).mult(o1, -8)
-
-.kaleid(2).color(0.3,0.5).rotate([40]).scale(1, 5, 2)
-  .mask(
-    osc(512,({time}) => Math.sin(time/24) + 0.25 ) // offset
-    .kaleid(3), 1, 0.9)// end mask
-  .modulateRotate(o2, 0.4)
-  .out(o3);
-osc([1.4], [2]).rotate([1.4], [.4].fast(0.5), [17], [2].fast(200)).blend(o1, 7)
-.blend(o3, 0.8).out(o0);
+osc([14], [2]).rotate([1.4], [.04].fast(0.05), [7], [0.02].fast(20)).blend(o1, 7).out(o2);
+osc(36).modulatePixelate(o1, 18).scrollX(0.3, [0.3, 3]).mult(o2, 256).mult(o1, -8).out(o0);
 
   let titleCanvasEl = document.getElementById('tbc')
   const titleBarHydra = new Hydra({
@@ -39,12 +30,13 @@ osc([1.4], [2]).rotate([1.4], [.4].fast(0.5), [17], [2].fast(200)).blend(o1, 7)
   });
 
   osc([24]).kaleid(2).color(0.3,0.5).rotate([40]).scale(1, 5, 0.8)
-  .kaleid(12).mask(
-    osc(512,({time}) => Math.sin(time/24) + 0.25 ) // offset
-    .kaleid(3), 1, 0.9)// end mask
-    .rotate(0.4)
-    .out(o1);
-osc([14], [2]).rotate([1.4], [.4].fast(0.5), [17], [2].fast(200)).blend(o1, 7).out(o2);
-osc([8]).blend(o2, 0.8).out(o0);
+    .kaleid(12).mask(
+      osc(512,({time}) => Math.sin(time/24) + 0.25 ) // offset
+      .kaleid(3), 1, 0.9)// end mask
+      .rotate(0.4)
+      .out(o1);
+  osc([14], [2]).rotate([1.4], [.4].fast(0.5), [17], [2].fast(200)).blend(o1, 7).out(o2);
+  osc([8]).blend(o2, 0.8).out(o0);
+  
 
 }
